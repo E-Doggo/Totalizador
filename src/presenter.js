6b6190193d7f960488f56056189d0d5ccf56db8e
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-08-11 09:20:17 -0400

    Se anadio el boton de multiplicar, se cambio el formato del html

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,29 @@
-import sumar from "./sumador";
+import {sumar,multiplicar} from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const sumBtn = document.querySelector("#SumBtn");
+const MultBtn = document.querySelector("#MultBtn")
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+
+sumBtn.addEventListener("click", ()=>{
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  console.log("funciona")
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+
+});
+
+MultBtn.addEventListener("click", () => {
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  console.log("funciona")
+
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
