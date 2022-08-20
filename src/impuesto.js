82caca4086f81f8a598d012e59b069523f3b5951
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-08-19 23:17:19 -0400

    Implementado seleccion de estado mediante texto

--- src/impuesto.js
+++ src/impuesto.js
@@ -1,14 +1,29 @@
+
+
 function impuestar(estado)
 {
     let tasaImp = 0;
     switch(estado)
     {
-        case 1:
+        case 'CA':
             tasaImp = 0.0825;
+            break;
+        case 'UT':
+            tasaImp = 0.0665;
+            break;
+        case 'NV':
+            tasaImp = 0.08;
+            break;
+        case 'TX':
+            tasaImp = 0.0625;
+            break;
+        case 'AL':
+            tasaImp = 0.04;
             break;
     }
     return tasaImp;
 }
+
 
 function calcularImpuesto(mult, tasaImp)
 {
--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import {impuestar, calcularImpuesto } from "./impuesto";
 
 const first = document.querySelector("#cantidad");
 const second = document.querySelector("#precio");
+const state = document.querySelector("#estado");
 const form = document.querySelector("#totalizar-form");
 const rDiv = document.querySelector("#resultado-div");
 const iDiv = document.querySelector('#impuesto-div');
@@ -15,10 +16,10 @@ MultBtn.addEventListener("click", () => {
   const secondNumber = Number.parseInt(second.value);
 
   let mult = multiplicar(firstNumber, secondNumber);
-  let imp = impuestar(1);
+  let imp = impuestar(state.value);
 
-  rDiv.innerHTML = "<p>" + "El precio neto es: " + mult + "</p>";
-  iDiv.innerHTML = "<p>" + "Impuesto CA: " + imp*100 +"%"+  "</p>";
-  tDiv.innerHTML = "<p>" + "total: " + calcularImpuesto(mult, imp) +  "</p>";
+  rDiv.innerHTML = "<p>" + "Precio neto = " + mult + "$ </p>";
+  iDiv.innerHTML = "<p>" + "Impuesto "+state.value+ " (" + imp*100 +"%) = "+ multiplicar(mult, imp) + "$ </p>";
+  tDiv.innerHTML = "<p>" + "Total = " + calcularImpuesto(mult, imp) +  "$ </p>";
   console.log('a');
 });
